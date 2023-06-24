4370b22e5b7563521a53e73baafb5c875e28b02a
Krishna B R <123768730+krish-na-br@users.noreply.github.com>
2023-06-24 10:54:50 +0530

    second

--- frontend/src/page/Contact.js
+++ frontend/src/page/Contact.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+
+const headingStyle = {
+  fontSize: '24px',
+  fontWeight: 'bold',
+};
+
+const paragraphStyle = {
+  fontSize: '16px',
+  lineHeight: '1.5',
+};
 
 export default function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div>
+      <p style={headingStyle}>Contact Number</p>
+      <p style={paragraphStyle}>
+        <strong>ARCHANA:</strong> 9400562619<br />
+        <strong>THANVIR:</strong> 9633009690<br />
+        <strong>KRISHNA PRADEEP:</strong> 6282334929<br />
+        <strong>NAYANA:</strong> 8590955473<br />
+        <strong>KRISHNA:</strong> 8590987104
+      </p>
+    </div>
+  );
 }
