12ee61b841d2952a7fafa33a3d16c59c2faf194b
14archana <120773603+14archana@users.noreply.github.com>
2023-06-24 21:32:07 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -1,93 +1,4 @@
-// const express=require('express')
-// const cors=require('cors')
-// const mongoose=require('mongoose')
-// const dotenv = require("dotenv").config();
-// // console.log("backend")
-// const app = express()
-// app.use(cors())
-// app.use(express.json({ limit: "10mb" }));
 
-// const PORT = process.env.PORT || 8080
-// //mongodb connection
-// mongoose.set("strictQuery", false);
-// mongoose.connect(process.env.MONGODB_URL)
-//   .then(() => console.log("Connect to Databse"))
-//   .catch((err) => console.log(err));
-
-//   //schema
-//   const userSchema = mongoose.Schema({
-//     firstName: String,
-//     lastName: String,
-//     email: {
-//       type: String,
-//       unique: true,
-//     },
-//     password: String,
-//     confirmPassword: String,
-//     image: String,
-//   });
-// //
-// const userModel = mongoose.model("user", userSchema);
-
-// // //api
-// //  app.get("/",(req,res)=>{
-// //     res.send("server is running")
-// //  })
-// //  app.post("/signup",(req,res)=>{
-// //     console.log(req.body)
-// //  })
-// //  app.listen(PORT,()=>console.log("server is running at port : " +PORT))
-
-// //api
-// app.get("/", (req, res) => {
-//    res.send("Server is running");
-//  });
- 
-//  //sign up
-//  app.post("/signup", async (req, res) => {
-//    // console.log(req.body);
-//    const { email } = req.body;
- 
-//    userModel.findOne({ email: email }, (err, result) => {
-//      // console.log(result);
-//      console.log(err);
-//      if (result) {
-//        res.send({ message: "Email id is already registered", alert: false });
-//      } else {
-//        const data = userModel(req.body);
-//        const save = data.save();
-//        res.send({ message: "Signed up successfully", alert: true });
-//      }
-//    });
-//  });
-
-//  //api login
-// app.post("/login", (req, res) => {
-//    // console.log(req.body);
-//    const { email } = req.body;
-//    userModel.findOne({ email: email }, (err, result) => {
-//      if (result) {
-//        const dataSend = {
-//          _id: result._id,
-//          firstName: result.firstName,
-//          lastName: result.lastName,
-//          email: result.email,
-//          image: result.image,
-//        };
-//        console.log(dataSend);
-//        res.send({
-//          message: "Login is successfully",
-//          alert: true,
-//          data: dataSend,
-//        });
-//      } else {
-//        res.send({
-//          message: "Email is not available, please sign up",
-//          alert: false,
-//        });
-//      }
-//    });
-//  });
 
 
 const express = require("express");
@@ -97,8 +8,7 @@ const dotenv = require("dotenv").config();
 const Stripe = require('stripe')
 
 const app = express();
-
-const corsOpts = { origin: '*', credentials: true, methods: ['GET','POST','HEAD','PUT','PATCH','DELETE'], allowedHeaders: ['Content-Type'], exposedHeaders: ['Content-Type'] }; app.use(cors(corsOpts));
+app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
 const PORT = process.env.PORT || 8080;
@@ -110,43 +20,38 @@ mongoose
   .then(() => console.log("Connect to Databse"))
   .catch((err) => console.log(err));
 
-//schema
-const userSchema = mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: {
-    type: String,
-    unique: true,
-  },
-  password: String,
-  confirmPassword: String,
-  image: String,
-});
 
-//
-const userModel = mongoose.model("user", userSchema);
 
 //api
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
+
+const userModel = require("./Model/user");
+
 //sign up
 app.post("/signup", async (req, res) => {
-  // console.log(req.body);
-  const { email } = req.body;
+  try {
+    const { email } = req.body;
+    console.log(email)
 
-  userModel.findOne({ email: email }, (err, result) => {
-    // console.log(result);
-    console.log(err);
-    if (result) {
-      res.send({ message: "Email id is already register", alert: false });
-    } else {
-      const data = userModel(req.body);
-      const save = data.save();
-      res.send({ message: "Successfully sign up", alert: true });
+    const doesExist = await userModel.findOne({ email: email })
+    if (doesExist) {
+      res.send({ message:"user already exists",status:400 })
+
     }
-  });
+    else {
+     const saveData =  await userModel(req.body)  //ERROR
+     const savedData = await saveData.save()
+     res.send({ message:"new user created",status:201 })
+
+    }
+
+  } catch (error) {
+    console.log(error)
+    res.send(error)
+  }
 });
 
 //api login
@@ -181,80 +86,81 @@ app.post("/login", (req, res) => {
 
 const schemaProduct = mongoose.Schema({
   name: String,
-  category:String,
+  category: String,
   image: String,
   price: String,
   description: String,
 });
-const productModel = mongoose.model("product",schemaProduct)
+const productModel = mongoose.model("product", schemaProduct)
 
 
 
 //save product in data 
 //api
-app.post("/uploadProduct",async(req,res)=>{
-    // console.log(req.body)
-    const data = await productModel(req.body)
-    const datasave = await data.save()
-    res.send({message : "Upload successfully"})
+app.post("/uploadProduct", async (req, res) => {
+  // console.log(req.body)
+  const data = await productModel(req.body)
+  const datasave = await data.save()
+  res.send({ message: "Upload successfully" })
 })
 
 //
-app.get("/product",async(req,res)=>{
+app.get("/product", async (req, res) => {
   const data = await productModel.find({})
   res.send(JSON.stringify(data))
 })
- 
+
 /*****payment getWay */
 console.log(process.env.STRIPE_SECRET_KEY)
 
 
-const stripe  = new Stripe(process.env.STRIPE_SECRET_KEY)
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
-app.post("/create-checkout-session",async(req,res)=>{
+app.post("/create-checkout-session", async (req, res) => {
 
-     try{
-      const params = {
-          submit_type : 'pay',
-          mode : "payment",
-          payment_method_types : ['card'],
-          billing_address_collection : "auto",
-          shipping_options : [{shipping_rate : "shr_1N0qDnSAq8kJSdzMvlVkJdua"}],
+  try {
+    const params = {
+      submit_type: 'pay',
+      mode: "payment",
+      payment_method_types: ['card'],
+      billing_address_collection: "auto",
+      shipping_options: [{ shipping_rate: "shr_1N0qDnSAq8kJSdzMvlVkJdua" }],
 
-          line_items : req.body.map((item)=>{
-            return{
-              price_data : {
-                currency : "inr",
-                product_data : {
-                  name : item.name,
-                  // images : [item.image]
-                },
-                unit_amount : item.price * 100,
-              },
-              adjustable_quantity : {
-                enabled : true,
-                minimum : 1,
-              },
-              quantity : item.qty
-            }
-          }),
+      line_items: req.body.map((item) => {
+        return {
+          price_data: {
+            currency: "inr",
+            product_data: {
+              name: item.name,
+              // images : [item.image]
+            },
+            unit_amount: item.price * 100,
+          },
+          adjustable_quantity: {
+            enabled: true,
+            minimum: 1,
+          },
+          quantity: item.qty
+        }
+      }),
 
-          success_url : `${process.env.FRONTEND_URL}/success`,
-          cancel_url : `${process.env.FRONTEND_URL}/cancel`,
+      success_url: `${process.env.FRONTEND_URL}/success`,
+      cancel_url: `${process.env.FRONTEND_URL}/cancel`,
 
-      }
+    }
 
-      
-      const session = await stripe.checkout.sessions.create(params)
-      // console.log(session)
-      res.status(200).json(session.id)
-     }
-     catch (err){
-        res.status(err.statusCode || 500).json(err.message)
-     }
+
+    const session = await stripe.checkout.sessions.create(params)
+    // console.log(session)
+    res.status(200).json(session.id)
+  }
+  catch (err) {
+    res.status(err.statusCode || 500).json(err.message)
+  }
 
 })
 
 
 //server is ruuning
 app.listen(PORT, () => console.log("server is running at port : " + PORT));
+
--- frontend/src/page/Signup.js
+++ frontend/src/page/Signup.js
@@ -16,7 +16,7 @@ function Signup() {
     email: "",
     password: "",
     confirmPassword: "",
-    image : ""
+    image: "",
   });
 
   const handleShowPassword = () => {
@@ -36,42 +36,41 @@ function Signup() {
     });
   };
 
-  const handleUploadProfileImage = async(e)=>{
-      const data = await ImagetoBase64(e.target.files[0])
-  
+  const handleUploadProfileImage = async (e) => {
+    const data = await ImagetoBase64(e.target.files[0]);
 
-      setData((preve)=>{
-          return{
-            ...preve,
-            image : data
-          }
-      })
-
-  }
-console.log(process.env.REACT_APP_SERVER_DOMIN)
-  const handleSubmit = async(e) => {
+    setData((preve) => {
+      return {
+        ...preve,
+        image: data,
+      };
+    });
+  };
+  console.log(process.env.REACT_APP_SERVER_DOMIN);
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const { firstName, email, password, confirmPassword } = data;
     if (firstName && email && password && confirmPassword) {
       if (password === confirmPassword) {
-    
-          const fetchData = await fetch(`${process.env.REACT_APP_SERVER_DOMIN}/signup`,{
-            method : "POST",
-            headers : {
-              "content-type" : "application/json"
+        const fetchData = await fetch(
+          `${process.env.REACT_APP_SERVER_DOMIN}/signup`,
+          {
+            method: "POST",
+            headers: {
+              "content-type": "application/json",
             },
-            body : JSON.stringify(data)
-          })
+            body: JSON.stringify(data),
+          }
+        );
 
-          const dataRes = await fetchData.json()
-    
-
-        // alert(dataRes.message);
-        toast(dataRes.message)
-        if(dataRes.alert){
+        const dataRes = await fetchData.json();
+        console.log("return value", dataRes);
+        if (dataRes.status === 201) {
+          toast(dataRes.message);
+        } else if (dataRes.status === 400) {
+          toast(dataRes.message);
           navigate("/login");
         }
-       
       } else {
         alert("password and confirm password not equal");
       }
@@ -85,13 +84,22 @@ console.log(process.env.REACT_APP_SERVER_DOMIN)
       <div className="w-full max-w-sm bg-white m-auto flex  flex-col p-4">
         {/* <h1 className='text-center text-2xl font-bold'>Sign up</h1> */}
         <div className="w-20 h-20 overflow-hidden rounded-full drop-shadow-md shadow-md m-auto relative ">
-          <img src={data.image ? data.image :  loginSignupImage} className="w-full h-full" />
+          <img
+            src={data.image ? data.image : loginSignupImage}
+            className="w-full h-full"
+          />
 
           <label htmlFor="profileImage">
             <div className="absolute bottom-0 h-1/3  bg-slate-500 bg-opacity-50 w-full text-center cursor-pointer">
               <p className="text-sm p-1 text-white">Upload</p>
             </div>
-            <input type={"file"} id="profileImage" accept="image/*" className="hidden" onChange={handleUploadProfileImage}/>
+            <input
+              type={"file"}
+              id="profileImage"
+              accept="image/*"
+              className="hidden"
+              onChange={handleUploadProfileImage}
+            />
           </label>
         </div>
 
